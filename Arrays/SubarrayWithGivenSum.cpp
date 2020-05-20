c4efcda86b322e303fbefb12c6296b199a65f780
Mukul Madaan <mukul.madaan8@gmail.com>
2020-05-21 02:40:32 +0530

    Update SubarrayWithGivenSum.cpp

--- Arrays/SubarrayWithGivenSum.cpp
+++ Arrays/SubarrayWithGivenSum.cpp
@@ -1,3 +1,5 @@
+//TLE via map
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -33,4 +35,38 @@ int main() {
 	    cout<<endl;
 	}
 	return 0;
+}
+
+//working code
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n,x;
+    int arr[10005];
+    cin>>t;
+    while(t--){
+        cin>>n>>x;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int start = 0;
+        int sum = arr[0];
+        int flag=0;
+        for(int i=1;i<=n && flag == 0;i++){
+
+            while(sum > x && start < i-1){
+                sum = sum - arr[start];
+                start++;
+            }
+            if(sum == x){
+                cout<<start+1<<" "<<i<<endl;
+                flag=1;
+            }
+            if(i < n)
+                sum += arr[i];
+        }
+        if(flag == 0)cout<<"-1\n";
+
+    }
+    return 0;
 }
