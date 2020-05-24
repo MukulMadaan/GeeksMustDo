4ff869822a59e5ad01d93ac892e3ff2f94115520
Mukul Madaan <mukul.madaan8@gmail.com>
2020-05-24 21:55:39 +0530

    Create ChocolateDistributionProblem.cpp

---
+++ Arrays/ChocolateDistributionProblem.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	int n;
+	while(t--){
+	    cin>>n;
+	    vector<int> arr;
+	    for(int i = 0 ; i < n; i++){
+	        int x;
+	        cin>>x;
+	        arr.push_back(x);
+	    }
+	    int m;
+	    cin>>m;
+	    sort(arr.begin(), arr.end());
+	    int res = INT_MAX;
+	    for(int i = 0 ; i <= n - m; i++){
+	        int x = arr[i + m - 1] - arr[i];
+	        res = min(res, x);
+	    }
+	    cout<<res<<endl;
+	}
+	return 0;
+}
