b6861fe090352e55e1785285a89fd5465da74b8f
Mukul Madaan <mukul.madaan8@gmail.com>
2020-06-02 19:14:04 +0530

    Update DetectCycleInDirectedGraph.cpp

--- Graphs/DetectCycleInDirectedGraph.cpp
+++ Graphs/DetectCycleInDirectedGraph.cpp
@@ -1,3 +1,31 @@
+//Solution2
+bool dfs(int v,vector<int> adj[], vector<int> &visited, vector<int> &recStack){
+    
+    bool ans = false;
+    visited[v] = 1;
+    recStack[v] = 1;
+    
+    for(int i = 0; i < adj[v].size(); i++){
+        if(visited[adj[v][i]] == 0){
+            ans = ans || dfs(adj[v][i], adj, visited, recStack);     
+        } else if(recStack[adj[v][i]])
+            return true;
+    }
+    recStack[v] = 0;
+    return ans;
+}
+bool isCyclic(int V, vector<int> adj[])
+{
+    vector<int> visited(V, 0);
+    vector<int> recStack(V, 0);
+    for(int i = 0; i < V; i++){
+        if(dfs(i,adj,visited, recStack))
+            return true;
+    }
+    return false;
+}
+
+//Solution1
 bool dfs(int v,vector<int> adj[], vector<int>&visited,int s){
     if(visited[v] == 1){
        return true; 
