7a8b857db9f20cb3fe7a8be0bf473372903a7501
Mukul Madaan <mukul.madaan8@gmail.com>
2020-06-08 10:18:38 +0530

    Create RepeatedCharacter.cpp

---
+++ Strings/RepeatedCharacter.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    string s;
+	    cin>>s;
+	    unordered_map<char, int> M;
+	    int n = s.length();
+	    for(int i = 0 ; i < n ; i++){
+	        M[s[i]]++;
+	    }
+	    int flag = 0;
+	    for(int i = 0 ; i < n ; i++){
+	        if(M[s[i]] > 1){
+	            cout<<s[i]<<endl;
+	            flag = 1;
+	            break;
+	        }
+	    }
+	    if(flag == 0) cout<<"-1"<<endl;
+	}
+	return 0;
+}
