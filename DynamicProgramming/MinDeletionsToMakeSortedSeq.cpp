f6f85b1f0ea8974a036cb81b64c5ecef55023717
Mukul Madaan <mukul.madaan8@gmail.com>
2020-06-19 12:30:15 +0530

    Create MinDeletionsToMakeSortedSeq.cpp

---
+++ DynamicProgramming/MinDeletionsToMakeSortedSeq.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i = 0; i < n; i++){
+            cin>>arr[i];	  
+	    }
+	    vector<int> dp(n, 1);
+	    for(int i = 1 ;i < n; i++){
+	        for(int j = 0; j < i; j++){
+	            if(arr[i] > arr[j]) dp[i] = max(dp[i], dp[j] + 1);
+	        }
+	    }
+	    cout<<n - *max_element(dp.begin(), dp.end())<<endl;
+	}
+	return 0;
+}
