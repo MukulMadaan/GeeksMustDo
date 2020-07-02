e2419f8cf14aa9e10775043bbd01b42b1f667eba
Mukul Madaan <mukul.madaan8@gmail.com>
2020-07-02 20:21:24 +0530

    Create InorderSuccessorBST.cpp

---
+++ Trees/InorderSuccessorBST.cpp
@@ -0,0 +1,15 @@
+Node * inOrderSuccessor(Node *root, Node *x)
+{
+    if(!root) return NULL;
+    
+    Node* y = NULL;
+    if(root->data<x->data)  y = inOrderSuccessor(root->right, x);
+    else if(root->data>x->data){
+        y = inOrderSuccessor(root->left, x);
+        if(y) y = root;
+    }else {
+        y = inOrderSuccessor(root->right, x);
+    }
+    
+    return y;
+}
