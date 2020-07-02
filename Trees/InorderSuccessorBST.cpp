5e69407db74ab0ac91b0fea8c75832d975abf472
Mukul Madaan <mukul.madaan8@gmail.com>
2020-07-02 20:22:15 +0530

    Update InorderSuccessorBST.cpp

--- Trees/InorderSuccessorBST.cpp
+++ Trees/InorderSuccessorBST.cpp
@@ -6,7 +6,7 @@ Node * inOrderSuccessor(Node *root, Node *x)
     if(root->data<x->data)  y = inOrderSuccessor(root->right, x);
     else if(root->data>x->data){
         y = inOrderSuccessor(root->left, x);
-        if(y) y = root;
+        if(!y) y = root;
     }else {
         y = inOrderSuccessor(root->right, x);
     }
